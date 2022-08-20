18d0ed5488e7a25649df15b24ffe57851c5b5425
Estrif <32181775+Estrif@users.noreply.github.com>
2022-08-20 13:23:00 -0700

    Update be_server.py

--- be_server.py
+++ be_server.py
@@ -2,4 +2,4 @@ from flask_app import app
 from flask_app.controllers import be_controller
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001, host="127.0.0.1")
+    app.run(debug=True, port=5000)
